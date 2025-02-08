07e8dbf16168bac721df67ce4dcd5445ba3c3d6d
Testspieler09 <142326461+Testspieler09@users.noreply.github.com>
2025-02-08 20:03:05 +0100

    vim-tmux-navigator added

--- init.lua
+++ init.lua
@@ -256,6 +256,25 @@ require('lazy').setup({
     vim.keymap.set('n', '<F5>', vim.cmd.UndotreeToggle),
   },
 
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
+
   { -- NOTE: Tagbar for displaying variable names etc.
     'preservim/tagbar',
     vim.keymap.set('n', '<F8>', vim.cmd.TagbarToggle),
