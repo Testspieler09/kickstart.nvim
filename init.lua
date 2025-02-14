a49bfeba3ede4d8e7eaddf20ddfa82bef134070a
Testspieler09 <142326461+Testspieler09@users.noreply.github.com>
2025-02-14 22:56:11 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -257,6 +257,17 @@ require('lazy').setup({
   },
 
   {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup {
+        '*', -- Highlight all files, but customize some others.
+        '!vim', -- Exclude vim from highlighting.
+        -- Exclusion Only makes sense if '*' is specified!
+      }
+    end,
+  },
+
+  {
     'christoomey/vim-tmux-navigator',
     cmd = {
       'TmuxNavigateLeft',
